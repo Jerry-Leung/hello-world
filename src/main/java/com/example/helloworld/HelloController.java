8f504f5e64b4e0423c22ab0475bcb6fbdbcab339
Ka Lok Leung <jerry@ikigaidigital.io>
2026-09-20 18:46:21 +0800

    read HELLO_MESSAGE from env var

--- src/main/java/com/example/helloworld/HelloController.java
+++ src/main/java/com/example/helloworld/HelloController.java
@@ -1,14 +1,18 @@
 package com.example.helloworld;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${HELLO_MESSAGE:Hello World 5}")
+    private String message;
+
     @GetMapping("/")
     public String hello() {
-        return "Hello World 5";
+        return message;
     }
 
 }
