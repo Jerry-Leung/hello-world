c0401ff08bbd5e2d598e5111574ba7cf698d638c
Ka Lok Leung <jerry@ikigaidigital.io>
2026-09-20 16:13:09 +0800

    Hello World 4

--- src/main/java/com/example/helloworld/HelloController.java
+++ src/main/java/com/example/helloworld/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World 3";
+        return "Hello World 4";
     }
 
 }
--- src/test/java/com/example/helloworld/HelloControllerTest.java
+++ src/test/java/com/example/helloworld/HelloControllerTest.java
@@ -19,7 +19,7 @@ class HelloControllerTest {
     void returnsHelloWorld() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello World 3"));
+                .andExpect(content().string("Hello World 4"));
     }
 
 }
